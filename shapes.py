7432db5d6cdda946a1b349539f352d242b3b3c7c
mbuguapurity <mbuguapurity1@gmail.com>
2019-04-30 19:34:08 +0300

    add files

---
+++ shapes.py
@@ -0,0 +1,64 @@
+class Circle:
+    def __int__(self,r):
+       self.r = r
+       
+
+    def Area(self):
+        area = 3.14*r*r
+        for m in area:
+             print(m)
+        
+      
+     
+
+    def Circumference(self,b):
+        Circumference = 2*3.14*r
+         
+        for b in circumference:
+           
+           print (b)
+
+    def __int__(self,a):
+         self.a = a
+         
+
+    def Area(self):
+         Area = a*a
+         for b in Area:
+          
+            print(b)
+
+    def __int__(self,w,l):
+        self.w = w
+        self.l = l
+
+    def AREA(self,l,w):
+        Area = W *l
+
+    def Periemeter(self,l,w):
+        periemeter = 2(L+w)
+        
+            
+          
+
+    def __int(self,R):
+        self.R = R
+
+
+    def SurfaceArea(self,R):
+        SurfaceArea  = 4*3.14*R*R
+
+
+    def volume(self,r):
+        volume = 4/3(3.14*r*r)
+        
+
+
+
+
+
+
+        
+
+
+     
