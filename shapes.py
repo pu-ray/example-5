70bb43066a342967a33174fb5f733410d1fd4e4d
mbuguapurity <mbuguapurity1@gmail.com>
2019-05-02 13:07:40 +0300

    add files

--- shapes.py
+++ shapes.py
@@ -4,53 +4,66 @@ class Circle:
        
 
     def Area(self):
-        area = 3.14*r*r
-        for m in area:
-             print(m)
+        r = self.r
+        area = 3.14*(r*r)
+      
+        print(area)
         
       
      
 
-    def Circumference(self,b):
-        Circumference = 2*3.14*r
+    def Circumference(self):
+        r = self.r
+        Circumference = 2*3.14*(r)
          
-        for b in circumference:
+        
            
-           print (b)
+        print (Circumference)
 
-    def __int__(self,a):
-         self.a = a
+class squares:
+
+    def __int__(self,side):
+          self.side = side
          
 
     def Area(self):
-         Area = a*a
-         for b in Area:
-          
-            print(b)
+          side = self.side
+          area = side*side
+          print(area)
 
-    def __int__(self,w,l):
-        self.w = w
-        self.l = l
+    def periemeter(self):
+        side = self.side
+        p = 4*side
+        print(p)
 
-    def AREA(self,l,w):
-        Area = W *l
+class rectangles:
 
-    def Periemeter(self,l,w):
-        periemeter = 2(L+w)
+
+    # def __int__(self,w,l):
+    #     self.w = w
+    #     self.l = l
+
+    # def AREA(self,l,w):
+    #     Area = W *l
+
+    # def Periemeter(self,l,w):
+    #     periemeter = 2(L+w)
+
+
         
             
           
-
-    def __int(self,R):
-        self.R = R
-
-
-    def SurfaceArea(self,R):
-        SurfaceArea  = 4*3.14*R*R
+class spheres:
+    # def __int(self,R):
+    #     self.R = R
 
 
-    def volume(self,r):
-        volume = 4/3(3.14*r*r)
+    # def SurfaceArea(self,R):
+    #     SurfaceArea  = 4*3.14*R*R
+
+
+    # def volume(self,r):
+    #     volume = 4/3(3.14*r*r)
         
 
 
@@ -61,4 +74,4 @@ class Circle:
         
 
 
-     
+    #  
